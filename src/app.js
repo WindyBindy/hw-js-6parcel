5c0d252d945f44982cf91555a798414a0e3afd2b
WindyBindy <nikita2021novsk@gmail.com>
2025-11-01 07:34:30 +0100

    small calc

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 console.log('Hello world!')
+import { add, minus, divide, multiply } from './js/calculator'
+console.log(add(23, 532))
+console.log(minus(246, 24))
+console.log(divide(64, 2))
+console.log(multiply(8, 3))
---
+++ src/js/calculator.js
@@ -0,0 +1,12 @@
+export function add(a, b) {
+	return 'add:' + a + b
+}
+export function minus(a, b) {
+	return 'minus:' + (a - b)
+}
+export function divide(a, b) {
+	return 'divide:' + a / b
+}
+export function multiply(a, b) {
+	return 'multiply:' + a * b
+}
