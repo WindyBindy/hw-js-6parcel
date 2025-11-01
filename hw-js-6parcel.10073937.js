76e1ddb4c3fe8439bf193048d97fed5c0a9ef29e
WindyBindy <nikita2021novsk@gmail.com>
2025-11-01 07:38:29 +0100

    Updates

---
+++ hw-js-6parcel.10073937.js
@@ -0,0 +1,2 @@
+console.log("Hello world!"),console.log("add:23532"),console.log("minus:222"),console.log("devide:32"),console.log("multiply:24");
+//# sourceMappingURL=hw-js-6parcel.10073937.js.map
--- hw-js-6parcel.813fa556.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world!");
-//# sourceMappingURL=hw-js-6parcel.813fa556.js.map
